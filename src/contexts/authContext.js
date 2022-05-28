83b1a8741cc951fe2570c77b7dbdf6800ecdf36b
Sankalp Sharma <sansharma056@gmail.com>
2022-05-28 14:06:54 +0530

    Removed unused keys

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,9 +1,7 @@
 import { createContext } from "react";
 
 export const AuthContext = createContext({
-  isAuthenticated: false,
   token: "",
-  name: "",
   signin: () => {},
   signout: () => {},
 });
