d982a0b5febae136551efd12bf9102ab70c1bf7a
Sankalp Sharma <sansharma056@gmail.com>
2022-05-28 14:11:00 +0530

    Addded routes, providing value to AuthContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "../contexts/authContext";
+import Cart from "./Cart";
+import Categories from "./Categories";
+import Checkout from "./Checkout";
 import Home from "./Home";
 import Login from "./Login";
+import ProductDetails from "./ProductDetails";
+import Products from "./Products";
 import Register from "./Register";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  function signin(token) {
+    localStorage.setItem("token", token);
+    setToken(token);
+  }
+
+  function signout() {
+    localStorage.removeItem("token");
+    setToken(null);
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ token, signin, signout }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="/" element={<Categories />} />
+            <Route path="/products/:id" element={<Products />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
